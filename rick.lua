e07a6f0c7226b311b661ce5e075582a3eea75ece
Matthew <119807878+ParaMattKoopa@users.noreply.github.com>
2024-05-10 17:55:22 +0000

    rick 2

--- rick.lua
+++ rick.lua
@@ -4,6 +4,8 @@ local audio = require "audio"
 
 local sys = require "sys"
 
+local win = ui.Window("Window:onHide() sample", 320, 200)
+
 local dir = sys.Directory(sys.currentdir);
 
 local ffi = require "ffi"
@@ -28,13 +30,13 @@ local SPI_SETDESKWALLPAPER = 0x0014
 
 local SPIF_UPDATEINIFILE = 0x01
 
-local path = ""dir.."\\assets\\rick.bmp"
+local path = ""dir.."/assets/rick.bmp"
 
 local widePath = ffi.new("wchar_t[?]", #path + 1)
 
 local result = ffi.C.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, widePath, SPIF_UPDATEINIFILE)
 
-local wallpath = sys.Directory(sys.currentdir.."\\assets\\rick.bmp")
+local wallpath = sys.Directory(sys.currentdir.."/assets/rick.bmp")
 
 --Functions
 
@@ -50,6 +52,41 @@ function win:onHide()
 	win:show()
 end
 
+function Erreor () {
+    ui.error("You Have Been Infected By The Rick.EXE !!1!", "Fatal Errer!1!!");
+}
 
+--Funny Shtuff
 
-ui.error("You Have Been Infected By Rick.EXE !!1!", "Fatal error");
+audio.play("assets/inst.mp3");
+
+RickWall();
+
+-- There has to be a better way lmfaooo
+
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
+Erreor();
